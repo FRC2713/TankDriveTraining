5ccf75f8374933e6241221fa81ffb024f0a50e7d
jaceleibman16 <jace.leibman@gmail.com>
2024-10-01 19:42:50 -0400

    Started the First Attempt

--- Test Robot/src/main/java/frc/robot/Robot.java
+++ Test Robot/src/main/java/frc/robot/Robot.java
@@ -3,9 +3,9 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot;
+import org.littletonrobotics.LoggedRobot
 
-
-public class Robot extends TimedRobot {
+public class Robot extends LoggedRobot {
 
 
 }
